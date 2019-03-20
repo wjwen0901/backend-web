8e56bbfe3092e20b7f52bf4b513455896a30ac71
daisly <leamoqinger_2011@163.com>
2019-03-20 17:28:35 +0800

    update:修改配置文件

--- genetic-consultaion/config/dev.env.js
+++ genetic-consultaion/config/dev.env.js
@@ -4,7 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_URL: '"http://localhost:9100/mdhcare-backend"'
-  BASE_URL: '"https://qa.mdhcare.cn/mdhcare-backend"'
+  BASE_URL: '"http://localhost:9100/mdhcare-backend"',
+  DISEASE_API: '"https://qa.mdhcare.cn/mini-gene"'
+  // BASE_URL: '"https://qa.mdhcare.cn/mdhcare-backend"'
   // BASE_URL: '"https://www.mdhcare.cn/mdhcare-backend"'
 })
--- genetic-consultaion/config/prod.env.js
+++ genetic-consultaion/config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://www.mdhcare.cn/mdhcare-backend"'
+  BASE_URL: '"https://www.mdhcare.cn/mdhcare-backend"',
+  DISEASE_API: '"https://www.mdhcare.cn/mini-gene/"'
   // BASE_URL: '"https://qa.mdhcare.cn/mdhcare-backend"'
 }
