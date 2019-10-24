4d0584e1e2ba6afbeabebfcca60b2ba9c8117d53
tqqweb <1462409700@qq.com>
2019-10-24 19:00:57 +0800

    诺康PDF

--- pdf-creater/child-drugs/genessential-pdf.js
+++ pdf-creater/child-drugs/genessential-pdf.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer'); 
-const axios  = require('axios');
+const puppeteer = require('puppeteer');
+const axios = require('axios');
 const OSS = require('ali-oss');
 const path = require("path");
 const fs = require("fs");
@@ -10,105 +10,105 @@ const BASE_URL_API = "https://www.mdhcare.cn/mdhcare-backend/";
 const BASE_URL = "https://z.mdhcare.cn/";
 
 const args = process.argv.splice(2);
-let names = args[1].substring(1, args[1].length-2).split(',');
+let names = args[1].substring(1, args[1].length - 2).split(',');
 const filesName = names;
 const reportId = args[0];
 
 
 
 //循环执行生成PDF文件
-filesName.forEach((item,index)=>{
-  (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    const pathName = "/data/mdh/pdf/customizedreport/";
-    await page.goto(BASE_URL + 'customized-pdf/#/child-drug?objectId=' + item,
-      {
-        waitUntil: 'networkidle0',
-        timeout: 0
-      });
-    await page.pdf({
-      path: pathName+item+'.pdf',
-      format: 'A4',
-      printBackground: true,
-    });
-  
-    await browser.close();
-
-    let accessid='';
-    let host= '';
-    let policyBase64= '';
-    let signature= '';
-    let callbackbody= '';
-    let key= '';
-    let g_object_name= '';
-    let g_object_name_type= '';
-    let now= Date.parse(new Date()) / 1000;
-    let uniqueKey= '';
-    let fileList= [];
-    let uploader= {};
-    axios.get(BASE_URL_API + 'oss/upload/policy/report?userId=1').then(res => {
-      let obj = res.data;
-      host = obj.host;
-      policyBase64 = obj.policy;
-      accessid = obj.accessid;
-      signature = obj.signature;
-      expire = parseInt(obj.expire, 10);
-      callbackbody = obj.callback;
-      key = obj.dir;
-      uniqueKey = obj.uniqueKey;
-
-
-
-      var states = fs.statSync(path.join(pathName, item+'.pdf'))
-      if(states.isFile()) {
-        let client = new OSS({
-          region: 'oss-cn-beijing',
-          //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
-          accessKeyId: 'LTAI3MjDjTuHs7ou',
-          accessKeySecret: 'pJflKHXWtSLHliDPgtzCb9slZ3hjIT',
-          bucket: 'mdhcare'
+filesName.forEach((item, index) => {
+    (async() => {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+        const pathName = "/data/mdh/pdf/customizedreport/";
+        await page.goto(BASE_URL + 'customized-pdf/#/child-drug?objectId=' + item, {
+            waitUntil: 'networkidle0',
+            timeout: 0
+        });
+        await page.pdf({
+            path: pathName + item + '.pdf',
+            format: 'A4',
+            printBackground: true,
         });
 
-        console.log(client);
-        let newName = key + uniqueKey +'.pdf';
-        console.log(newName);
-        console.log(path.join(pathName, item+'.pdf'));
-        async function put() {
-          try {
-            // object表示上传到OSS的Object名称，localfile表示本地文件或者文件路径
-            let r1 = await client.put(newName, path.join(pathName, item+'.pdf'));
-            console.log('put success: %j', r1);
-            // let r2 = await client.get(newName);
-            // console.log('get success: %j', r2);
+        await browser.close();
 
-            //上传成功，保存记录
-            const params = {
-              fileName: path.join(item+'.pdf'),
-              size: states.size,
-              mimeType: 'application/pdf',
-              uniqueKey: uniqueKey,
-              filePath: newName,
-              objectKey: newName,
-              xlsReportId: parseInt(reportId)
+        let accessid = '';
+        let host = '';
+        let policyBase64 = '';
+        let signature = '';
+        let callbackbody = '';
+        let key = '';
+        let g_object_name = '';
+        let g_object_name_type = '';
+        let now = Date.parse(new Date()) / 1000;
+        let uniqueKey = '';
+        let fileList = [];
+        let uploader = {};
+        axios.get(BASE_URL_API + 'oss/upload/policy/report?userId=1').then(res => {
+            let obj = res.data;
+            host = obj.host;
+            policyBase64 = obj.policy;
+            accessid = obj.accessid;
+            signature = obj.signature;
+            expire = parseInt(obj.expire, 10);
+            callbackbody = obj.callback;
+            key = obj.dir;
+            uniqueKey = obj.uniqueKey;
+
+
+
+            var states = fs.statSync(path.join(pathName, item + '.pdf'))
+            if (states.isFile()) {
+                let client = new OSS({
+                    region: 'oss-cn-beijing',
+                    //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
+                    accessKeyId: 'LTAI3MjDjTuHs7ou',
+                    accessKeySecret: 'pJflKHXWtSLHliDPgtzCb9slZ3hjIT',
+                    bucket: 'mdhcare'
+                });
+
+                console.log(client);
+                let newName = key + uniqueKey + '.pdf';
+                console.log(newName);
+                console.log(path.join(pathName, item + '.pdf'));
+                async function put() {
+                    try {
+                        // object表示上传到OSS的Object名称，localfile表示本地文件或者文件路径
+                        let r1 = await client.put(newName, path.join(pathName, item + '.pdf'));
+                        console.log('put success: %j', r1);
+                        // let r2 = await client.get(newName);
+                        // console.log('get success: %j', r2);
+
+                        //上传成功，保存记录
+                        const params = {
+                            fileName: path.join(item + '.pdf'),
+                            size: states.size,
+                            mimeType: 'application/pdf',
+                            uniqueKey: uniqueKey,
+                            filePath: newName,
+                            objectKey: newName,
+                            xlsReportId: parseInt(reportId)
+                        }
+                        console.log('--------------------------------------');
+                        console.log(params);
+                        axios({
+                            url: BASE_URL_API + 'customize/childrenDrug',
+                            method: 'post',
+                            params: params
+                        }).then(res => {
+                            console.log(res.data)
+                        })
+                    } catch (e) {
+                        console.error('error: %j', err);
+                    }
+                }
+
+                put();
             }
-            console.log('--------------------------------------');
-            console.log(params);
-            axios({
-              url: BASE_URL_API + 'customize/childrenDrug',
-              method: 'post',
-              params: params
-            }).then(res => {
-              console.log(res.data)
-            })
-          } catch (e) {
-            console.error('error: %j', err);
-          }
-        }
 
-        put();
-      }
+        })
 
-    })
-  })();
+    })();
 });
--- pdf-creater/nuokang/pdf_nuokang.js
+++ pdf-creater/nuokang/pdf_nuokang.js
@@ -1,37 +1,117 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const OSS = require('ali-oss');
+const path = require("path");
+const fs = require("fs");
 
-const args = process.argv.splice(2)
+const BASE_URL_API = "https://qa.mdhcare.cn/mdhcare-backend/";
+const BASE_URL = "https://qa.mdhcare.cn/";
+
+// const BASE_URL_API = "https://www.mdhcare.cn/mdhcare-backend/";
+// const BASE_URL = "https://z.mdhcare.cn/";
+
+const args = process.argv.splice(2);
 console.log(args);
-console.log(args[1]);
-console.log(args[1]);
+console.log(args[0]);
 
-(async () => {
-  // 在浏览器上设置的一组可配置选项
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-
-  //返回一个新的Page对象。Page 在一个默认的浏览器上下文中被创建。
-  const page = await browser.newPage();
-
-  const pageInfo = 'https://z.mdhcare.cn/#/customize/genessential/' + (args[1] === 'skin' ? 'pdf-nuokang' : 'sport-report') + '/' + args[0];
-  //console.log(pageInfo);
-
-  // page.goto(url[, options])抛出或返回主页面的响应:url:导航到的地址;timeout:跳转等待时间(毫秒);waitUntil:满足什么条件认为页面跳转完成;networkidle0:不再有网络连接时触发（至少500毫秒后）
-  await page.goto(pageInfo,
-    {
-      waitUntil: 'networkidle0',
-      timeout: 0
+(async() => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    const pathName = "/data/mdh/pdf/nuokang/";
+    await page.goto(BASE_URL + 'customized-pdf/#/nuokang?objectStr=' + args[0], {
+        waitUntil: 'networkidle0',
+        timeout: 0
+    });
+    // console.log(pathName + args[0] + '.pdf')
+    await page.pdf({
+        path: pathName + args[0] + '.pdf',
+        format: 'A4',
+        printBackground: true,
     });
 
-  //生成当前页面的pdf格式: path:pdf文件保存的路径;printBackground:是否打印背景图;format:页面格式;
-  await page.pdf({
-    path: '/data/mdh/pdf/genessential/' + args[0] + '.pdf',
-    format: 'A4',
-    printBackground: true
-  });
-  // 关闭 Chromium 及其所有页面(如果页面被打开的话)
-  await browser.close();
+    await browser.close();
 
-  // console.log('http://localhost:3100/#/customize/genessential/' + args[1] == 'skin' ? 'pdf-nuokang' : 'sport-report' + '/' + args[0]);
-  // console.log('/Users/leamoliu/mdhcare/data/mdh/pdf/genessential/' + args[0] + '.pdf');
-  // console.log('end');
+    let accessid = '';
+    let host = '';
+    let policyBase64 = '';
+    let signature = '';
+    let callbackbody = '';
+    let key = '';
+    let g_object_name = '';
+    let g_object_name_type = '';
+    let now = Date.parse(new Date()) / 1000;
+    let uniqueKey = '';
+    let fileList = [];
+    let uploader = {};
+    axios.get(BASE_URL_API + 'oss/upload/policy/report?userId=1').then(res => {
+            // console.log(res)
+            let obj = res.data;
+            host = obj.host;
+            policyBase64 = obj.policy;
+            accessid = obj.accessid;
+            signature = obj.signature;
+            expire = parseInt(obj.expire, 10);
+            callbackbody = obj.callback;
+            key = obj.dir;
+            uniqueKey = obj.uniqueKey;
+
+
+
+            var states = fs.statSync(path.join(pathName, args[0] + '.pdf'))
+                // console.log(states)
+            if (states.isFile()) {
+                let client = new OSS({
+                    region: 'oss-cn-beijing',
+                    //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
+                    accessKeyId: 'LTAI3MjDjTuHs7ou',
+                    accessKeySecret: 'pJflKHXWtSLHliDPgtzCb9slZ3hjIT',
+                    bucket: 'mdhcare',
+                    timeout: 180000
+                });
+
+                // console.log(client);
+                let newName = key + uniqueKey + '.pdf';
+                // console.log(newName);
+                // console.log(path.join(pathName, args[0] + '.pdf'));
+                async function put() {
+                    try {
+                        // object表示上传到OSS的Object名称，localfile表示本地文件或者文件路径
+                        let r1 = await client.put(newName, path.join(pathName, args[0] + '.pdf'));
+                        // console.log('put success: %j', r1);
+                        // let r2 = await client.get(newName);
+                        // console.log('get success: %j', r2);
+
+                        //上传成功，保存记录
+                        const params = {
+                                fileName: path.join(args[0] + '.pdf'),
+                                size: states.size,
+                                mimeType: 'application/pdf',
+                                uniqueKey: uniqueKey,
+                                filePath: newName,
+                                objectKey: newName,
+                                userId: 1,
+                                name: '易得好康超级管理员',
+                                cellphone: '100000001'
+                            }
+                            // console.log('--------------------------------------');
+                            // console.log(params);
+                        axios({
+                            url: BASE_URL_API + 'report/upload',
+                            method: 'post',
+                            params: params
+                        }).then(res => {
+                            console.log(res.data)
+                        })
+                    } catch (err) {
+                        console.error('error: %j', err);
+                    }
+                }
+
+                put();
+            }
+
+        })
+        // console.log('http://localhost:3100/#/customize/genessential/' + args[1] == 'skin' ? 'pdf-nuokang' : 'sport-report' + '/' + args[0]);
+        // console.log('/Users/leamoliu/mdhcare/data/mdh/pdf/genessential/' + args[0] + '.pdf');
+        // console.log('end');
 })();
