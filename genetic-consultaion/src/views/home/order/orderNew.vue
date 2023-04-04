ff4d94afcf20dcc88417fa099ac6dab7ac6f8af6
wjwen0901 <478817846@qq.com>
2023-04-04 19:45:29 +0800

    订单管理添加改价功能

--- genetic-consultaion/src/views/home/order/orderNew.vue
+++ genetic-consultaion/src/views/home/order/orderNew.vue
@@ -27,12 +27,15 @@
                         <template v-for="(item, index) in orderList">
                              <div :key="index+'&'" class="space"></div>
                              <tr :key="index" class="i-line">
-                                <td colspan="8">&nbsp;
+                                <td colspan="3">
                                     <svg t="1665285503374" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2676" width="16" height="16"><path d="M512 950.848c-242.368 0-438.848-196.48-438.848-438.848C73.152 269.632 269.632 73.152 512 73.152c242.368 0 438.848 196.48 438.848 438.848 0 242.368-196.48 438.848-438.848 438.848z m-18.976-491.84v-129.856a36.576 36.576 0 0 0-73.152 0v209.248L262.624 315.296a43.872 43.872 0 0 0-79.776 25.28v299.008a36.576 36.576 0 1 0 73.152 0v-206.72l157.28 223.072a43.872 43.872 0 0 0 79.744-25.28v-32.384a146.272 146.272 0 1 0 0-139.264z m128.704 142.752a73.152 73.152 0 1 1 0-146.272 73.152 73.152 0 0 1 0 146.272z m182.848 56.512a36.576 36.576 0 1 0 0-73.12 36.576 36.576 0 0 0 0 73.12z" p-id="2677" fill="#01806f"></path></svg>
-                                    订单编号：{{item.orderNo}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    报告时间：
-                                    <svg t="1665285864156" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8826" width="16" height="16"><path d="M512 512m-450.56 0a450.56 450.56 0 1 0 901.12 0 450.56 450.56 0 1 0-901.12 0Z" fill="#01806f" p-id="8827" data-spm-anchor-id="a313x.7781069.0.i10" class=""></path><path d="M491.52 317.44a30.72 30.72 0 0 1 30.57664 27.77088L522.24 348.16v174.08H757.76a30.72 30.72 0 0 1 30.57664 27.77088L788.48 552.96a30.72 30.72 0 0 1-27.77088 30.57664L757.76 583.68H491.52a30.72 30.72 0 0 1-30.57664-27.77088L460.8 552.96V348.16a30.72 30.72 0 0 1 30.72-30.72z" fill="#ffffff" p-id="8828" data-spm-anchor-id="a313x.7781069.0.i11" class="selected"></path></svg>                                    {{item.createTime | formatDate}}
+                                    订单编号：{{item.orderNo}}
                                 </td>
+                                <td colspan="3">
+                                    <svg t="1665285864156" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8826" width="16" height="16"><path d="M512 512m-450.56 0a450.56 450.56 0 1 0 901.12 0 450.56 450.56 0 1 0-901.12 0Z" fill="#01806f" p-id="8827" data-spm-anchor-id="a313x.7781069.0.i10" class=""></path><path d="M491.52 317.44a30.72 30.72 0 0 1 30.57664 27.77088L522.24 348.16v174.08H757.76a30.72 30.72 0 0 1 30.57664 27.77088L788.48 552.96a30.72 30.72 0 0 1-27.77088 30.57664L757.76 583.68H491.52a30.72 30.72 0 0 1-30.57664-27.77088L460.8 552.96V348.16a30.72 30.72 0 0 1 30.72-30.72z" fill="#ffffff" p-id="8828" data-spm-anchor-id="a313x.7781069.0.i11" class="selected"></path></svg>
+                                     报告时间：{{item.createTime | formatDate}}
+                                </td>
+                                <td colspan="2"></td>
                                 <td class="detail">
                                     <el-button type="text" size="small" @click="toInformedDetail(item.id,item.expressCode,item.expressId)">查看详情</el-button>
                                 </td>
@@ -47,8 +50,9 @@
                                     {{item.payment || '-'}}</td>
                                 <td>{{item.pName}}({{item.pCellphone || '-'}})</td>
                                 <td>{{item.fullName || '-'}}</td>
-                                <td>
+                                <td class="price-box">
                                     <el-tag size="small" effect="plain" :style="{color:getColor(item.statusStr),borderColor:getColor(item.statusStr)}">{{item.statusStr || '——'}}</el-tag>
+                                    <el-button v-if="item.statusStr==='收款码待付款'" type="text" size="small" class="priceText" @click="changePrice(item.payment,item.orderNo,item.itemTitle,item.id)">改价</el-button>
                                 </td>
                                 <td colspan="2">
                                     <el-button type="text" size="small" @click="toUploadInformed(item.id)">上传知情</el-button>
@@ -79,12 +83,83 @@
                 </el-pagination>
             </div>
         </div>
+        <el-dialog
+            title="改 价"
+            :visible.sync="visiablePrice"
+            center
+            class="price-dialog"
+            width="60%">
+            <el-row style="margin-top:20px; font-size: 15px;">
+                <svg t="1665285503374" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2676" width="16" height="16"><path d="M512 950.848c-242.368 0-438.848-196.48-438.848-438.848C73.152 269.632 269.632 73.152 512 73.152c242.368 0 438.848 196.48 438.848 438.848 0 242.368-196.48 438.848-438.848 438.848z m-18.976-491.84v-129.856a36.576 36.576 0 0 0-73.152 0v209.248L262.624 315.296a43.872 43.872 0 0 0-79.776 25.28v299.008a36.576 36.576 0 1 0 73.152 0v-206.72l157.28 223.072a43.872 43.872 0 0 0 79.744-25.28v-32.384a146.272 146.272 0 1 0 0-139.264z m128.704 142.752a73.152 73.152 0 1 1 0-146.272 73.152 73.152 0 0 1 0 146.272z m182.848 56.512a36.576 36.576 0 1 0 0-73.12 36.576 36.576 0 0 0 0 73.12z" p-id="2677" fill="#01806f"></path></svg>
+                <span>订单编号：</span>
+                <span>{{orderNo}}</span>
+            </el-row>
+            <el-row style="margin-top:10px; font-size: 15px;">
+                <svg t="1665285503374" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2676" width="16" height="16"><path d="M512 950.848c-242.368 0-438.848-196.48-438.848-438.848C73.152 269.632 269.632 73.152 512 73.152c242.368 0 438.848 196.48 438.848 438.848 0 242.368-196.48 438.848-438.848 438.848z m-18.976-491.84v-129.856a36.576 36.576 0 0 0-73.152 0v209.248L262.624 315.296a43.872 43.872 0 0 0-79.776 25.28v299.008a36.576 36.576 0 1 0 73.152 0v-206.72l157.28 223.072a43.872 43.872 0 0 0 79.744-25.28v-32.384a146.272 146.272 0 1 0 0-139.264z m128.704 142.752a73.152 73.152 0 1 1 0-146.272 73.152 73.152 0 0 1 0 146.272z m182.848 56.512a36.576 36.576 0 1 0 0-73.12 36.576 36.576 0 0 0 0 73.12z" p-id="2677" fill="#01806f"></path></svg>
+                <span>检测项目：</span>
+                <span>{{itemTitle}}</span>
+            </el-row>
+            <el-row type="flex" justify="center" class="priceDiag">
+                <el-col :span="6">
+                    <div class="priceBox">
+                        <span class="priceTitle">当前价格</span>
+                        <span class="linerText">
+                            <svg t="1662443559209" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12023" width="16" height="16"><path d="M512 0C228.8 0 0 228.8 0 512s228.8 512 512 512 512-228.8 512-512S795.2 0 512 0z m0 976C256 976 48 768 48 512S256 48 512 48s464 208 464 464-208 464-464 464z" fill="#F9AC3E" p-id="12024"></path><path d="M512 96C281.6 96 96 281.6 96 512s185.6 416 416 416 416-185.6 416-416S742.4 96 512 96z m209.6 171.2L544 464v28.8h150.4c14.4 0 25.6 11.2 25.6 25.6S707.2 544 692.8 544H544v54.4h150.4c14.4 0 25.6 11.2 25.6 25.6s-11.2 25.6-25.6 25.6H544v124.8-16c0 19.2-16 33.6-33.6 33.6-19.2 0-33.6-16-33.6-33.6v16-124.8H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V544H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V464l-176-196.8s-19.2-52.8 40-35.2c20.8 6.4 36.8 35.2 36.8 35.2l136 145.6 139.2-145.6s16-30.4 40-35.2c54.4-11.2 28.8 35.2 28.8 35.2z" fill="#F9AC3E" p-id="12025"></path></svg>
+                            {{nowPrice}}</span>
+                    </div>
+                </el-col>
+                <el-col :span="2">
+                    <div class="priceBox">
+                        <span class="priceTitle">+</span>
+                        <span class="linerText">+</span>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div class="priceBox">
+                        <span class="priceTitle">添加价格</span>
+                        <div>
+                            <svg t="1662443559209" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12023" width="16" height="16"><path d="M512 0C228.8 0 0 228.8 0 512s228.8 512 512 512 512-228.8 512-512S795.2 0 512 0z m0 976C256 976 48 768 48 512S256 48 512 48s464 208 464 464-208 464-464 464z" fill="#F9AC3E" p-id="12024"></path><path d="M512 96C281.6 96 96 281.6 96 512s185.6 416 416 416 416-185.6 416-416S742.4 96 512 96z m209.6 171.2L544 464v28.8h150.4c14.4 0 25.6 11.2 25.6 25.6S707.2 544 692.8 544H544v54.4h150.4c14.4 0 25.6 11.2 25.6 25.6s-11.2 25.6-25.6 25.6H544v124.8-16c0 19.2-16 33.6-33.6 33.6-19.2 0-33.6-16-33.6-33.6v16-124.8H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V544H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V464l-176-196.8s-19.2-52.8 40-35.2c20.8 6.4 36.8 35.2 36.8 35.2l136 145.6 139.2-145.6s16-30.4 40-35.2c54.4-11.2 28.8 35.2 28.8 35.2z" fill="#F9AC3E" p-id="12025"></path></svg>
+                            <el-input class="liner" v-model="addPrice" size="small" placeholder="请输入价格" style="width:140px;"></el-input>
+                        </div>
+                        <span v-if="addPrice && !/^(\-|\+)?\d+(\.\d+)?$/.test(addPrice)" class="price-tip">* 请输入正确的金额</span>
+                    </div>
+                </el-col>
+                <el-col :span="2">
+                    <div class="priceBox">
+                        <span class="priceTitle">=</span>
+                        <span class="linerText">=</span>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div class="priceBox">
+                        <span class="priceTitle">改后价格</span>
+                        <span class="linerText">
+                        <svg t="1662443559209" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12023" width="16" height="16"><path d="M512 0C228.8 0 0 228.8 0 512s228.8 512 512 512 512-228.8 512-512S795.2 0 512 0z m0 976C256 976 48 768 48 512S256 48 512 48s464 208 464 464-208 464-464 464z" fill="#F9AC3E" p-id="12024"></path><path d="M512 96C281.6 96 96 281.6 96 512s185.6 416 416 416 416-185.6 416-416S742.4 96 512 96z m209.6 171.2L544 464v28.8h150.4c14.4 0 25.6 11.2 25.6 25.6S707.2 544 692.8 544H544v54.4h150.4c14.4 0 25.6 11.2 25.6 25.6s-11.2 25.6-25.6 25.6H544v124.8-16c0 19.2-16 33.6-33.6 33.6-19.2 0-33.6-16-33.6-33.6v16-124.8H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V544H329.6c-14.4 0-25.6-11.2-25.6-25.6s11.2-25.6 25.6-25.6h147.2V464l-176-196.8s-19.2-52.8 40-35.2c20.8 6.4 36.8 35.2 36.8 35.2l136 145.6 139.2-145.6s16-30.4 40-35.2c54.4-11.2 28.8 35.2 28.8 35.2z" fill="#F9AC3E" p-id="12025"></path></svg>
+                        {{afterPrice}}</span>
+                        <span class="price-tip" v-if="afterPrice<0" style="transform: translateY(-30px);">* 赔钱啦~怎么变成负数啦~</span>
+                    </div>
+                </el-col>
+            </el-row>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="visiablePrice = false" size="small">取 消</el-button>
+                <el-button type="primary" @click="changePriceApi" size="small" :disabled="priceBut">确 定</el-button>
+            </span>
+            </el-dialog>
     </div>
 </template>
 <script>
+import NP from 'number-precision';
 export default {
     data(){
         return{
+            orderId:'',
+            itemTitle:'',
+            orderNo:'',
+            priceBut:true,
+            addPrice:'',//添加价格
+            nowPrice:'',//之前的价格
+            afterPrice:'',//改后价格
+            visiablePrice: false,
             isIndeterminate: false,
             checkAll:false,//全选
             checkIs:false,
@@ -135,6 +210,26 @@ export default {
             },
             deep: true
         },
+        //计算
+        addPrice(val){
+            if(val && !/^(\-|\+)?\d+(\.\d+)?$/.test(val)){
+                this.priceBut = true
+                return
+            }
+            this.priceBut = false
+            if(val && val.charAt(1)){
+                this.afterPrice = NP.plus(this.addPrice,this.nowPrice)//减法
+            }else{
+                this.afterPrice = this.nowPrice
+            }
+        },
+        afterPrice(val){
+            if(val<0) {
+                this.priceBut = true
+                return
+            }
+            this.priceBut = false
+         },
         pageNum(newNum,oldNum){
             const arrId = window.sessionStorage.getItem('checkPage')
             const obj = JSON.parse(arrId)
@@ -150,6 +245,34 @@ export default {
         }
     },
     methods:{
+        //改价接口
+        changePriceApi(){
+            this.axios.post('order/edit',{
+                id:this.orderId,
+                payment:this.afterPrice
+            }).then(res => {
+                if (res.data === 'success') {
+                this.$message({message: '改价成功', type: 'success'})
+                this.visiablePrice = false
+                this.getList()
+                } else {
+                this.$message({message: '改价失败', type: 'warning'})
+                }
+            }).catch(err => {
+                console.log(err)
+                this.visiablePrice = false
+            })
+        },
+        //价格弹窗
+        changePrice(val,orderNo,itemTitle,id){
+            this.orderId = id
+            this.nowPrice = val
+            this.orderNo = orderNo
+            this.itemTitle = itemTitle
+            this.afterPrice = this.nowPrice
+            this.addPrice = ''
+            this.visiablePrice = true
+        },
         //复选框
         handelCheckIs(val,item){
             item.isChecked = val
@@ -163,7 +286,6 @@ export default {
                 })
             }
             this.$forceUpdate()
-            console.log(this.checkIds)
         },
         //全选
         handleAllCheck(val){
@@ -386,8 +508,60 @@ export default {
             }
          }
     }
+    .priceDiag{
+        margin:30px 0 70px 0;
+        border: 1px dashed #e2f8f6;
+        padding: 60px 15px 37px 15px;
+        background: #e4edec;
+        border-radius:15px;
+        height: 250px;
+    }
+    .price-dialog{
+        svg{
+            transform: translateY(2px);
+        }
+    }
+    .priceBox{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .liner{
+            margin-top: 35px;
+        }
+        .linerText{
+            margin-top: 10px;
+            font-size: 16px;
+            line-height: 5.5;
+        }
+        .priceTitle{
+            font-size: 18px;
+        }
+        .price-tip{
+            font-size: 12px;
+            color: #bc0d0a;
+            line-height: 40px;
+        }
+    }
+    .price-box{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .el-tag{
+             margin-top: 5px;
+        }
+        .priceText{
+            color:rgb(16, 31, 251);
+            text-decoration: underline;
+        }
+    }
 </style>
 <style lang="scss">
-.order-box{
-}
+    .price-dialog{
+        .el-dialog__body{
+            background: #f5f6f9;
+        }
+        .el-dialog__header {
+            background: #e4edec;
+        }
+    }
 </style>
